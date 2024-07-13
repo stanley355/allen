b200b66a032f3dc82b059caa8ded3f865d901922
Stanley Winata <winatastanley355@gmail.com>
2024-07-13 10:00:41 +0700

    Fix naming

--- A1/multivariate/ga_count.py
+++ A1/multivariate/ga_count.py
@@ -3,16 +3,16 @@ from statsmodels.multivariate.manova import MANOVA
 
 # Sample data
 data = {
-    'Promotion': [0, 1, 0, 1, 0, 1],
-    'Total_Users': [1000, 1200, 1100, 1300, 1050, 1250],
-    'Event_Count': [5000, 6000, 5500, 6500, 5250, 6250],
-    'Event_Count_per_User': [5, 5, 5, 5, 5, 5]
+    'promotion': [0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+    'total_users': [1000, 1200, 1100, 1300, 1050, 1250],
+    'event_count': [5000, 6000, 5500, 6500, 5250, 6250],
+    'event_count_per_user': [5, 5, 5, 5, 5, 5]
 }
 
 df = pd.DataFrame(data)
 
 # Fit the multivariate regression model
-manova = MANOVA.from_formula('Total_Users + Event_Count + Event_Count_per_User ~ Promotion', data=df)
+manova = MANOVA.from_formula('total_users + event_count + event_count_per_user ~ promotion', data=df)
 result = manova.mv_test()
 
 print(result)
--- A1/multivariate/single_event.py
+++ A1/multivariate/single_event.py
@@ -3,16 +3,16 @@ from statsmodels.multivariate.manova import MANOVA
 
 # Sample data
 data = {
-    'Promotion': [0, 1, 0, 1, 0, 1],
-    'Total_Users': [1000, 1200, 1100, 1300, 1050, 1250],
-    'Event_Count': [5000, 6000, 5500, 6500, 5250, 6250],
-    'Event_Count_per_User': [5, 5, 5, 5, 5, 5]
+    'promotion': [0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+    'login': [1000, 1200, 1100, 1300, 1050, 1250],
+    'translate': [5000, 6000, 5500, 6500, 5250, 6250],
+    'checkbot': [5, 5, 5, 5, 5, 5]
 }
 
 df = pd.DataFrame(data)
 
 # Fit the multivariate regression model
-manova = MANOVA.from_formula('Total_Users + Event_Count + Event_Count_per_User ~ Promotion', data=df)
+manova = MANOVA.from_formula('login + translate + checkbot ~ promotion', data=df)
 result = manova.mv_test()
 
 print(result)
