e121104e9209323c2c90a0a7a05efed1be90a683
Stanley Winata <winatastanley355@gmail.com>
2024-07-13 10:17:53 +0700

    Update based on event

--- A1/wilcoxson/login.py
+++ A1/wilcoxson/login.py
@@ -2,14 +2,14 @@ import numpy as np
 from scipy.stats import wilcoxon
 
 # Pre experiment data
-pre_login = np.array([3, 1, 3, 5, 6])
-pre_translate = np.array([3, 3, 15, 1, 9])
-pre_checkbot = np.array([15, 11, 2, 4, 13])
+pre_total_user = np.array([3, 1, 3, 5, 6])
+pre_event_count = np.array([2,	1,	3,	2,	5])
+pre_event_count_per_user = np.array([1.5,	1,	1,	2.5,	1.2])
 
 # Post experiment data
-post_login = np.array([7, 8, 6, 5, 0])
-post_translate = np.array([12, 26, 19, 22, 0])
-post_checkbot = np.array([7, 26, 1, 16, 0])
+post_total_user = np.array([7, 8, 6, 5, 2])
+post_event_count = np.array([5,	5,	6,	3,	2])
+post_event_count_per_user = np.array([1.4,	1.6,	1,	1.67,	1])
 
 # Perform Wilcoxon signed-rank test
 def perform_wilcoxon_test(pre_data, post_data, variable_name):
@@ -20,6 +20,7 @@ def perform_wilcoxon_test(pre_data, post_data, variable_name):
     else:
         print(f"Result: {variable_name} does not show a statistically significant difference after the experiment.\n")
 
-perform_wilcoxon_test(pre_login, post_login, 'Login')
-perform_wilcoxon_test(pre_translate, post_translate, 'Translate')
-perform_wilcoxon_test(pre_checkbot, post_checkbot, 'Checkbot')
+print("Login")
+perform_wilcoxon_test(pre_total_user, post_total_user , 'Total User: ')
+perform_wilcoxon_test(pre_event_count, post_event_count, 'Event Count: ')
+perform_wilcoxon_test(pre_event_count_per_user, post_event_count_per_user, 'Event Count per User')
