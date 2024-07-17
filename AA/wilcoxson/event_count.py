16532bc42e0d83564b722cfe6b85337c931f48ae
Stanley Winata <winatastanley355@gmail.com>
2024-07-17 12:21:24 +0700

    Pre post

--- AA/wilcoxson/event_count.py
+++ AA/wilcoxson/event_count.py
@@ -1,9 +1,11 @@
 import pandas
-import scipy.stats as stats
+from scipy.stats import wilcoxon
 
-df = pandas.read_csv("AA/files/AA_pre_post - event_count.csv")
+df = pandas.read_csv("AA/files/AA_pre_post - All.csv")
 
-pre_translate = df['pre_translate']
-post_translate = df['post_translate']
-translate_wilcoxson = stats.wilcoxson(pre_translate, post_translate)
-print(translate_wilcoxson)
+translate_cols = df[['ec_pre_translate', 'ec_post_translate', 'tu_pre_translate', 'tu_post_translate', 'ecpe_pre_translate', 'ecpe_post_translate']]
+# post_translate = df['post_translate']
+# translate_wilcoxson = wilcoxon(pre_translate[0], pre_translate[0])
+
+print(translate_cols)
+# print(translate_wilcoxson)
