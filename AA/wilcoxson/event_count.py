8567d4dd61cd33483cd31f49727e71ac2e882cf1
Stanley Winata <winatastanley355@gmail.com>
2024-07-17 11:55:39 +0700

    Event count csv

--- AA/wilcoxson/event_count.py
+++ AA/wilcoxson/event_count.py
@@ -1,6 +1,9 @@
 import pandas
+import scipy.stats as stats
 
 df = pandas.read_csv("AA/files/AA_pre_post - event_count.csv")
 
-translate_data = df[['pre_translate', 'post_translate']]
-print(translate_data)
+pre_translate = df['pre_translate']
+post_translate = df['post_translate']
+translate_wilcoxson = stats.wilcoxson(pre_translate, post_translate)
+print(translate_wilcoxson)
