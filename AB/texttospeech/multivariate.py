5f95e9729c231e7e57dc55a8dc8e6443df3810b2
stan <winatastanley355@gmail.com>
2024-08-03 11:25:06 +0700

    TTS

--- AB/texttospeech/multivariate.py
+++ AB/texttospeech/multivariate.py
@@ -1,33 +1,33 @@
 import pandas
 from statsmodels.multivariate.manova import MANOVA
 
-df = pandas.read_csv("AB/students/AB_pre_post - students.csv")
+df = pandas.read_csv("AB/texttospeech/AB_pre_post - Text to Speech.csv")
 
-translate_data = {
+home_tts_data= {
     'experiment': [0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
-    'event_count': pandas.concat([df['ec_pre_translate'], df['ec_post_translate']]).values,
-    'total_users': pandas.concat([df['tu_pre_translate'], df['tu_post_translate']]).values,
-    'event_count_per_user': pandas.concat([df['ecpe_pre_translate'], df['ecpe_post_translate']]).values
+    'event_count': pandas.concat([df['ec_tts_home'], df['ec_tts']]).values,
+    'total_users': pandas.concat([df['tu_tts_home'], df['tu_tts']]).values,
+    'event_count_per_user': pandas.concat([df['ecpu_tts_home'], df['ecpu_tts']]).values
 }
 
-translate_df = pandas.DataFrame(translate_data)
-translate_manova = MANOVA.from_formula('total_users + event_count + event_count_per_user ~ experiment', translate_df)
-translate_result = translate_manova.mv_test()
+home_df = pandas.DataFrame(home_tts_data)
+home_manova = MANOVA.from_formula('total_users + event_count + event_count_per_user ~ experiment', home_df)
+home_result = home_manova.mv_test()
 
-checkbot_data = {
+students_data = {
     'experiment': [0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
-    'event_count': pandas.concat([df['ec_pre_checkbot'], df['ec_post_checkbot']]).values,
-    'total_users': pandas.concat([df['tu_pre_checkbot'], df['tu_post_checkbot']]).values,
-    'event_count_per_user': pandas.concat([df['ecpe_pre_checkbot'], df['ecpe_post_checkbot']]).values
+    'event_count': pandas.concat([df['ec_tts_students'], df['ec_tts']]).values,
+    'total_users': pandas.concat([df['tu_tts_students'], df['tu_tts']]).values,
+    'event_count_per_user': pandas.concat([df['ecpu_tts_students'], df['ecpu_tts']]).values
 }
 
-checkbot_df = pandas.DataFrame(checkbot_data)
-checkbot_manova = MANOVA.from_formula('total_users + event_count + event_count_per_user ~ experiment', checkbot_df)
-checkbot_result = checkbot_manova.mv_test()
+students_df = pandas.DataFrame(students_data)
+student_manova = MANOVA.from_formula('total_users + event_count + event_count_per_user ~ experiment', students_df)
+student_reulst = student_manova.mv_test()
 
-print("Translate")
-print(translate_result)
+print("HOME VS TTS")
+print(home_result)
 print('\n')
-print("Checkbot")
-print(checkbot_result)
+print("STUDENTS VS TTS")
+print(student_reulst)
 
--- AB/texttospeech/poisson.py
+++ AB/texttospeech/poisson.py
@@ -1,25 +1,46 @@
 import pandas
 import statsmodels.formula.api as smf
 
-df = pandas.read_csv("AB/texttospeech/AB_pre_post - texttospeech.csv")
+df = pandas.read_csv("AB/texttospeech/AB_pre_post - Text to Speech.csv")
 
-tts_home_students_data = {
+home_tts_data= {
     'experiment': [0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
-    'event_count': pandas.concat([df['ec_pre_translate'], df['ec_post_translate']]).values,
-    'total_users': pandas.concat([df['tu_pre_translate'], df['tu_post_translate']]).values,
-    'event_count_per_user': pandas.concat([df['ecpe_pre_translate'], df['ecpe_post_translate']]).values
+    'event_count': pandas.concat([df['ec_tts_home'], df['ec_tts']]).values,
+    'total_users': pandas.concat([df['tu_tts_home'], df['tu_tts']]).values,
+    'event_count_per_user': pandas.concat([df['ecpu_tts_home'], df['ecpu_tts']]).values
 }
 
-translate_df = pandas.DataFrame(translate_data)
-ec_translate_model = smf.poisson("event_count ~ experiment", translate_df).fit()
-tu_translate_model = smf.poisson("total_users ~ experiment", translate_df).fit()
-ecpe_translate_model = smf.poisson("event_count_per_user ~ experiment", translate_df).fit()
+home_df = pandas.DataFrame(home_tts_data)
+ec_home = smf.poisson("event_count ~ experiment", home_df).fit()
+tu_home = smf.poisson("total_users ~ experiment", home_df).fit()
+ecpu_home = smf.poisson("event_count_per_user ~ experiment", home_df).fit()
 
-print("Translate")
+print("HOME VS TTS")
 print("EVENT COUNT:")
-print(ec_translate_model.summary())
-print("EVENT COUNT:")
-print(tu_translate_model.summary())
+print(ec_home.summary())
+print("TOTAL_USERS:")
+print(tu_home.summary())
 print("EVENT COUNT PER USERS:")
-print(ecpe_translate_model.summary())
+print(ecpu_home.summary())
 
+
+students_data = {
+    'experiment': [0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+    'event_count': pandas.concat([df['ec_tts_students'], df['ec_tts']]).values,
+    'total_users': pandas.concat([df['tu_tts_students'], df['tu_tts']]).values,
+    'event_count_per_user': pandas.concat([df['ecpu_tts_students'], df['ecpu_tts']]).values
+}
+
+students_df = pandas.DataFrame(students_data)
+ec_students = smf.poisson("event_count ~ experiment", students_df).fit()
+tu_students= smf.poisson("total_users ~ experiment", students_df).fit()
+ecpu_students = smf.poisson("event_count_per_user ~ experiment", students_df).fit()
+
+print("\n\n")
+print("STUDENTS VS TTS")
+print("EVENT COUNT:")
+print(ec_students.summary())
+print("TOTAL_USERS:")
+print(tu_students.summary())
+print("EVENT COUNT PER USERS:")
+print(ecpu_students.summary())
