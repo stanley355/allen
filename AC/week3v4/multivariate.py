a4f7cad492c929e7b885a18ebdd42512cde01c8f
stan <winatastanley355@gmail.com>
2024-08-24 11:33:27 +0700

    Week3v4

--- AC/week3v4/multivariate.py
+++ AC/week3v4/multivariate.py
@@ -5,14 +5,14 @@ df = pandas.read_csv("AC/AC.csv")
 
 data= {
     'experiment': [0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
-    'event_count': pandas.concat([df['week1_ec'], df['week2_ec']]).values,
-    'total_users': pandas.concat([df['week1_tu'], df['week2_tu']]).values,
-    'event_count_per_user': pandas.concat([df['week1_ecpu'], df['week2_ecpu']]).values
+    'event_count': pandas.concat([df['week3_ec'], df['week4_ec']]).values,
+    'total_users': pandas.concat([df['week3_tu'], df['week4_tu']]).values,
+    'event_count_per_user': pandas.concat([df['week3_ecpu'], df['week4_ecpu']]).values
 }
 
 data_df = pandas.DataFrame(data)
 data_manova = MANOVA.from_formula('total_users + event_count + event_count_per_user ~ experiment', data_df).mv_test()
 
-print("Week 1 vs Week 2: Multivariate")
+print("Week3 vs Week 4: Multivariate")
 print(data_manova)
 
--- AC/week3v4/poisson.py
+++ AC/week3v4/poisson.py
@@ -7,9 +7,9 @@ df = pandas.read_csv("AC/AC.csv")
 
 data= {
     'experiment': [0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
-    'event_count': pandas.concat([df['week1_ec'], df['week2_ec']]).values,
-    'total_users': pandas.concat([df['week1_tu'], df['week2_tu']]).values,
-    'event_count_per_user': pandas.concat([df['week1_ecpu'], df['week2_ecpu']]).values
+    'event_count': pandas.concat([df['week3_ec'], df['week4_ec']]).values,
+    'total_users': pandas.concat([df['week3_tu'], df['week4_tu']]).values,
+    'event_count_per_user': pandas.concat([df['week3_ecpu'], df['week4_ecpu']]).values
 }
 
 data_df = pandas.DataFrame(data)
@@ -17,7 +17,7 @@ ec_sum = smf.poisson("event_count ~ experiment", data_df).fit().summary()
 tu_sum = smf.poisson("total_users ~ experiment", data_df).fit().summary()
 ecpu_sum = smf.poisson("event_count_per_user ~ experiment", data_df).fit().summary()
 
-print("WEEK 1 VS WEEK 2")
+print("WEEK3 VS WEEK4")
 print("EVENT COUNT:")
 print(ec_sum)
 print("TOTAL USERS:")
